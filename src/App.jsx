78ae4d2670f02b6e362790fd2af143710589736e
Teerathep <Teerathep.ng@gmail.com>
2024-11-01 11:50:51 +0700

    create three first nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Navbar from './components/Navbar';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+// import { Outlet } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 
 export default function App() {
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-      Hello world!
-        </h1>
-        <a>
-          asd
-        </a>
+      <div>
+        <BrowserRouter>
+        <Navbar />
+          <Routes>
+            <Route path='/' element={<Home />}/>
+            <Route path='/about' element={<About />}/>
+            <Route path='/contact' element={<Contact />}/>
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
 
-  )
+  );
 }
 
 /*
